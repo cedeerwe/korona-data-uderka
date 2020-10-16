65ef6cee7996120233eda76c55b22ef4b9c4ddd8
Kiko Čabala <kristiancabala@gmail.com>
2020-10-16 23:39:45 +0200

    analysis

--- analysis.py
+++ analysis.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-df = pd.read_csv("pop.csv")
+df = pd.read_csv("pop_fixed.csv")
 
-print(df[df['population'] > 500])
+pop_key = 'pocet_obyvatelov_2013'
+city_cap = 3000
+total_residents = df[pop_key].sum()
+
+living_in_big_cities = df[df[pop_key] > city_cap][pop_key].sum()
+
+ratio = living_in_big_cities / total_residents
+
+print(total_residents)
+print("{}% lives in cities where pop(city) > {}".format(ratio * 100, city_cap))
+
